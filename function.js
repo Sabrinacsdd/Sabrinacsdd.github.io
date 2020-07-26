b76ed219c94532ea453c25ea93e2166eba411d02
Sabrinacsdd <Sabrinacsdd@users.noreply.github.com>
2020-07-26 15:35:13 -0700

    debug

--- function.js
+++ function.js
@@ -9,6 +9,8 @@ var changeScene = (i) => {
 
     var data = getData(curDate);
 
+    clearTimeout(timeIndex1);
+    clearTimeout(timeIndex2);
     //update x y domain
     x.domain(data.map(function (d) { return d.OS; }));
     y.domain([0, 100]);
@@ -17,9 +19,7 @@ var changeScene = (i) => {
     xAxis.call(d3.axisBottom(x).tickFormat(function (d) { return d; }));
     yAxis.call(d3.axisLeft(y).tickFormat(x => `${x}%`));
 
-    // chart.text(curDate)
-    // clearTimeout(timeIndex1);
-    // clearTimeout(timeIndex2);
+  
     //remove annotations
     d3.selectAll('.labels').remove();
     
@@ -48,40 +48,9 @@ var changeScene = (i) => {
         // .attr("height", function (d) { return height - y(0); })
         .attr("class", function (d) { return d.OS; })
         .attr("fill", " #6a5acd")
-        .on('end', () => {
-            //add annotations accordingly
-            if (curDate == dateSet[0]) {
-                drawAnnotation('SymbianOS', 'Leading position');
-                drawAnnotation('iOS', 'iphone4 release');
-                d3.select('#conclusion')
-                    .text('Before the Android world ruled Smartphones, one of the most widely-adopted operating systems for phones was the Symbian OS. The platform was popular up until 2010 when Google’s Android gave it a run for its money. In February 2011, after Nokia’s newly appointed CEO announced that the company was partnering with Microsoft to develop Nokia devices running Windows, the Symbian platform gradually dropped. Developers deserted the ecosystem rapidly.')
-            }
-            if (curDate == dateSet[1]) {
-                drawAnnotation('iOS', 'iphone5 release');
-                drawAnnotation('Android', 'Surpass SymbianOs');
-                d3.select('#conclusion')
-                    .text('Android and iOs start to have more market share than SymbianOs.     Nokia releases the Nokia 808 PureView,later confirmed (in January 2013) to be the last Symbian smartphone.')
-
-            }
-            if (curDate == dateSet[2]) {
-                drawAnnotation('BlackBerry', 'BlackBerry 10 release');
-                drawAnnotation('Android', 'Leading position');
-                d3.select('#conclusion')
-                    .text('Android start taking the lead position in mobile OS market share. BlackBerry releases their new operating system for smartphones, BlackBerry 10. Google releases Android KitKat 4.4.')
-            }
-            if (curDate == dateSet[3]) {
-                drawAnnotation('iOS', 'Apple watch release');
-                drawAnnotation('Android', 'Leading position');
-                d3.select('#conclusion')
-                    .text('Android takes the leading position in mobile market share. Google releases Android 6.0 "Marshmallow". BlackBerry announces that there are no plans to release new APIs and software development kits for BlackBerry 10, and future updates would focus on security and privacy enhancements only.')
-            }
-            if (curDate == dateSet[4]) {
-                drawAnnotation('iOS', 'Stable market share');
-                drawAnnotation('Android', 'Leading position');
-                d3.select('#conclusion')
-                    .text('Today, Android is the most widely used mobile operation system. iOS has the second largest installed base worldwide on smartphones, but the largest profits, due to aggressive price competition between Android-based manufacturers.')
-            }
-        });
+        // .on('end', () => {
+            
+        // });
     // .on("mousemove", function (d) {
     //     tooltip
     //         .style("left", d3.event.pageX - 50 + "px")
@@ -132,7 +101,38 @@ var changeScene = (i) => {
             }
         })
     }
+    //add annotations accordingly
+    if (curDate == dateSet[0]) {
+        timeIndex1 = setTimeout(() => drawAnnotation('SymbianOS', 'Leading position'), 1000);
+        timeIndex2 = setTimeout(() => drawAnnotation('iOS', 'iphone4 release'), 1000);
+        d3.select('#conclusion')
+            .text('Before the Android world ruled Smartphones, one of the most widely-adopted operating systems for phones was the Symbian OS. The platform was popular up until 2010 when Google’s Android gave it a run for its money. In February 2011, after Nokia’s newly appointed CEO announced that the company was partnering with Microsoft to develop Nokia devices running Windows, the Symbian platform gradually dropped. Developers deserted the ecosystem rapidly.')
+    }
+    if (curDate == dateSet[1]) {
+        timeIndex1 = setTimeout(() => drawAnnotation('iOS', 'iphone5 release'), 1000);
+        timeIndex2 = setTimeout(() => drawAnnotation('Android', 'Surpass SymbianOs'), 1000);
+        d3.select('#conclusion')
+            .text('Android and iOs start to have more market share than SymbianOs.     Nokia releases the Nokia 808 PureView,later confirmed (in January 2013) to be the last Symbian smartphone.')
 
+    }
+    if (curDate == dateSet[2]) {
+        timeIndex1 = setTimeout(() => drawAnnotation('BlackBerry', 'BlackBerry 10 release'), 1000);
+        timeIndex2 = setTimeout(() => drawAnnotation('Android', 'Leading position'), 1000);
+        d3.select('#conclusion')
+            .text('Android start taking the lead position in mobile OS market share. BlackBerry releases their new operating system for smartphones, BlackBerry 10. Google releases Android KitKat 4.4.')
+    }
+    if (curDate == dateSet[3]) {
+        timeIndex1 = setTimeout(() => drawAnnotation('iOS', 'Apple watch release'), 1000);
+        timeIndex2 = setTimeout(() => drawAnnotation('Android', 'Leading position'), 1000);
+        d3.select('#conclusion')
+            .text('Android takes the leading position in mobile market share. Google releases Android 6.0 "Marshmallow". BlackBerry announces that there are no plans to release new APIs and software development kits for BlackBerry 10, and future updates would focus on security and privacy enhancements only.')
+    }
+    if (curDate == dateSet[4]) {
+        timeIndex1 = setTimeout(() => drawAnnotation('iOS', 'Stable market share'), 1000);
+        timeIndex2 = setTimeout(() => drawAnnotation('Android', 'Leading position'), 1000);
+        d3.select('#conclusion')
+            .text('Today, Android is the most widely used mobile operation system. iOS has the second largest installed base worldwide on smartphones, but the largest profits, due to aggressive price competition between Android-based manufacturers.')
+    }
 
 }
 
@@ -145,14 +145,14 @@ var checkClick = (cb) => {
 }
 
 var reset = () => {
+    clearTimeout(timeIndex1);
+    clearTimeout(timeIndex2);
     d3.selectAll("input")
         .property('checked', true);
     for (var i in active) {
         active[i] = true;
     }
-    // chart.text(curDate)
-    clearTimeout(timeIndex1);
-    clearTimeout(timeIndex2);
+    
     index = -1;
     d3.selectAll('.labels').remove();
     d3.selectAll("rect")
@@ -163,5 +163,7 @@ var reset = () => {
         .text(' ')
     d3.select('#showDate')
         .text('Please use the "next" button to follow narrative visualization or use the number button to choose any slide you want.');
-    // changeScene(0);
+    
+    
+    
 }
